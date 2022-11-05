b0ed1e49b61e717b9565ef28bafb21ebdaba4425
tadekradek <105873923+tadekradek@users.noreply.github.com>
2022-11-05 13:10:05 +0100

    Further development in Django, edits to my_site added.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
+urlpatterns = [  #for every url pattern that you are adding here, you need to have respective view logic
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page") #/posts/my-first-post - this is a concept called slug
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,45 @@
+from datetime import date
+
 from django.shortcuts import render
 
+all_posts = [
+    {"slug":"ligunia",
+     "img":"liga.png",
+     "author":"radek",
+     "date": date(2022,11,5),
+     "title":"Liga Legend",
+     "excert":"Welcome to the league of Draven",
+     "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit"},
+    {"slug":"spotify",
+     "img":"spotify.png",
+     "author":"radek",
+     "date": date(2022,11,6),
+     "title":"Spotify",
+     "excert":"Spotify Logo",
+     "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit"},
+    {"slug":"github",
+     "img":"github.png",
+     "author":"radek",
+     "date": date(2022,11,7),
+     "title":"Github Desktop",
+     "excert":"Github Desktop Icon",
+     "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
+]
+
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key = get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {"posts": latest_posts})
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    sorted_posts = sorted(all_posts, key = get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/all-posts.html",{"posts": latest_posts})
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):  #if you have dynamic url in urlpatters with slug, this function needs to receive another argument
+    return render(request, "blog/post-detail.html")
